c8bd2d661174d6dcf859ed0dfb81e16a39ef91c1
Agustina Agatiello <aagatiello@alumnos.nebrija.es>
2021-02-25 16:36:52 +0100

    ejercicio 22/2 - personajes r&m

---
+++ ejercicio2/index.js
@@ -0,0 +1,58 @@
+var pag = 1;
+
+const cargarListado = () => {
+  fetch("https://rickandmortyapi.com/api/character?page=" + pag)
+    .then((response) => response.json())
+    .then((data) => fillData(data));
+};
+
+const fillData = (data) => {
+  var html = "";
+  data.results.forEach((elem) => {
+    html += "<div class = 'character'>";
+    html += "<div id = 'profilepic'>";
+    html += "<img src='" + elem.image + "'alt='profile img'/>";
+    html += "</div>";
+    html += "<div class = 'description'>";
+    html += "<div id = 'name'>" + elem.name + "</div>";
+    html += "<div id = 'status'>";
+    if (elem.status === "Alive") var status = "status_alive";
+    if (elem.status === "Dead") var status = "status_dead";
+    if (elem.status === "unknown") var status = "status_unknown";
+    html +=
+      "<div class =" +
+      status +
+      "></div>" +
+      elem.status +
+      " - " +
+      elem.species +
+      "</div>";
+    html += "<div id='origin'> Origin: <br>" + elem.origin.name + "</div>";
+    html +=
+      "<div id='lastloc'> Last known location: <br>" +
+      elem.location.name +
+      "</div>";
+    html += "</div></div>";
+  });
+  document.getElementById("listado").innerHTML = html;
+};
+
+function prevPage() {
+  pag = pag - 1;
+  if (pag <= 1) {
+    pag = 0;
+    document.getElementById("prevnum").style.visibility = "hidden";
+  }
+  if (pag < 34) document.getElementById("nextnum").style.visibility = "visible";
+  cargarListado(pag);
+}
+
+function nextPage() {
+  pag = pag + 1;
+  if (pag >= 34) {
+    pag = 34;
+    document.getElementById("nextnum").style.visibility = "hidden";
+  }
+  if (pag > 1) document.getElementById("prevnum").style.visibility = "visible";
+  cargarListado(pag);
+}
